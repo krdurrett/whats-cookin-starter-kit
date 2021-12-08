2d10f70e551d16c5075608a4313dbe775dfd0bd2
Kayla Durrett <kayla.durrett@gmail.com>
2021-12-07 18:48:47 -0700

    Add new file for test purposes and start test file for RecipeRepository

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,20 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import recipeData from '../src/data/recipes-test';
 
 describe('Recipe', () => {
+  let recipeRepository;
+  beforeEach(() => {
+    recipeRepository = new RecipeRepository(recipeData);
+  });
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
+  it('Should instantiate a new instance of recipeRepository', () => {
+    expect(recipeRepository).to.be.an.instanceOf(RecipeRepository);
+  });
+
+  
 })
