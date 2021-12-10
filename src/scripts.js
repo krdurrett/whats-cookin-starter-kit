4c286e5eb58c54da6a05c63d5af9a317e8c8afec
Jasmyn Hicks <jasmynh44@gmail.com>
2021-12-09 18:35:03 -0700

    Add exports, classes, and a display all recipes funtion

--- src/scripts.js
+++ src/scripts.js
@@ -2,32 +2,53 @@ import './styles.css';
 import apiCalls from './apiCalls';
 import RecipeRepository from './classes/RecipeRepository';
 import Recipe from './classes/Recipe';
-import { recipeData } from './data/recipes';
-import { ingredientsData } from './data/ingredients';
+import recipeData from './data/recipes';
+import ingredientsData from './data/ingredients';
 
 //Query Selectors
 const allRecipesButton = document.querySelector('#allRecipesButton');
 const recipeCardSection = document.querySelector('#recipeCardSection');
+const landingPageView = document.querySelector('#landingPageView');
+const recipeDisplayView = document.querySelector('#recipeDisplayView');
 
 //Global variables
 let recipeRepository;
 
 //Funtions
+const addHidden = elements => {
+  elements.forEach(item => {
+    item.classList.add("hidden");
+  })
+}
+
+const removeHidden = elements => {
+  elements.forEach(item => {
+    item.classList.remove("hidden");
+  })
+}
+
 const instantiateRecipeRepository = () => {
-  new RecipeRepository(recipeData, ingredientsData);
+  recipeRepository = new RecipeRepository(recipeData, ingredientsData);
 }
 
 
+
 const displayAllRecipes = () => {
-  recipeCardSection.innerHTML = `
-  <section class="recipe-card">
-    <img class="recipe-card-img" src="https://spoonacular.com/recipeImages/595736-556x370.jpg">
-    <button>Recipe Name</button>
-    <div class="recipe-card-buttons">
-      <button>🥘</button>
-      <button>❤️</button>
-    </div>
-  </section>`;
+  addHidden([landingPageView]);
+  removeHidden([recipeDisplayView]);
+  recipeCardSection.innerHTML = ``;
+  recipeRepository.recipes.forEach(recipe => {
+    recipeCardSection.innerHTML += `
+    <section class="recipe-card">
+      <img class="recipe-card-img" src="${recipe.image}">
+      <button>${recipe.name}</button>
+      <div class="recipe-card-buttons">
+        <button>🥘</button>
+        <button>❤️</button>
+      </div>
+    </section>`;
+  })
+
 }
 
 console.log('Hello world');
