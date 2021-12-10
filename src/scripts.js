004b4140e2fa24907368f6fb8f3da7bbf4fe6a84
Jasmyn Hicks <jasmynh44@gmail.com>
2021-12-09 18:02:42 -0700

    Import files, instantiate RecipeRepository, add event listener for all recipes button

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,37 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+import RecipeRepository from './classes/RecipeRepository';
+import Recipe from './classes/Recipe';
+import { recipeData } from './data/recipes';
+import { ingredientsData } from './data/ingredients';
+
+//Query Selectors
+const allRecipesButton = document.querySelector('#allRecipesButton');
+const recipeCardSection = document.querySelector('#recipeCardSection');
+
+//Global variables
+let recipeRepository;
+
+//Funtions
+const instantiateRecipeRepository = () => {
+  new RecipeRepository(recipeData, ingredientsData);
+}
 
 
-
+const displayAllRecipes = () => {
+  recipeCardSection.innerHTML = `
+  <section class="recipe-card">
+    <img class="recipe-card-img" src="https://spoonacular.com/recipeImages/595736-556x370.jpg">
+    <button>Recipe Name</button>
+    <div class="recipe-card-buttons">
+      <button>🥘</button>
+      <button>❤️</button>
+    </div>
+  </section>`;
+}
 
 console.log('Hello world');
+
+//Event Listeners
+window.addEventListener('load', instantiateRecipeRepository);
+allRecipesButton.addEventListener('click', displayAllRecipes)
