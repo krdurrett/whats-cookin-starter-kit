30b2b9d0e18f574a30a6e4fcb9c0cb5d506260ac
Kayla Durrett <kayla.durrett@gmail.com>
2021-12-12 09:21:29 -0700

    Add User fetch request to apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,10 @@
-// Your fetch requests will live here!
+export const fetchAllUsers = () => {
+    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+      .then(response => response.json())
+      .then(data => data.users)
+      .catch(err => console.log('Oops! Something went wrong', err))
+}
+
 
 
 console.log('I will be a fetch request!')
