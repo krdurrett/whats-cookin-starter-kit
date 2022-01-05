f9dd326792c2214ed1750b753c953153f5a3434c
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-01-04 20:08:54 -0700

    Update urls for api request for user, recipe and ingredients. Update promise.all to reflect api changes

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,17 @@
 export const fetchAllUsers = () => {
-    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+    return fetch('http://localhost:3001/api/v1/users')
       .then(response => response.json())
       .catch(err => console.log('Oops! Something went wrong', err))
 }
 
 export const fetchAllRecipes = () => {
-  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
+  return fetch('http://localhost:3001/api/v1/recipes')
     .then(response => response.json())
     .catch(err => console.log('Oops! Something went wrong', err))
 }
 
 export const fetchAllIngredients = () => {
-  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
+  return fetch('http://localhost:3001/api/v1/ingredients')
     .then(response => response.json())
     .catch(err => console.log('Oops! Something went wrong', err))
 }
